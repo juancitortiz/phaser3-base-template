dc5234df3ef23b29d0a17aeac335056386a98efd
Juan Ignacio Ortiz Innocenti <juanitolaguna1993@gmail.com>
2024-10-04 22:39:36 -0300

    level0 load correctly

--- src/scenes/bootScene.js
+++ src/scenes/bootScene.js
@@ -4,19 +4,23 @@ class BootScene extends Phaser.Scene {
     }
   
     preload() {
-      this.load.path = "./assets/";
-  
-      let loadingBar = this.add.graphics()
-      .fillGradientStyle(0xffffff, 0xee9900, 0xffffff, 0xee9900, 1);
-  
-      this.load.on('progress', (percentaje) => {
-        loadingBar.fillRect(0, this.game.config.height / 2, this.game.config.width * percentaje, 50);
-      })
-  
-      this.load.on('complete', () => {
-        console.log("cargo!");
-        this.scene.start("level0");
-      })
+        // It needs to load something or it will not load the next scene.
+        this.load.setBaseURL('https://labs.phaser.io');
+        this.load.image('logo', 'assets/sprites/phaser3-logo.png');
+
+        this.load.path = "./assets/";
+    
+        let loadingBar = this.add.graphics()
+        .fillGradientStyle(0xffffff, 0xee9900, 0xffffff, 0xee9900, 1);
+    
+        this.load.on('progress', (percentaje) => {
+            loadingBar.fillRect(0, this.game.config.height / 2, this.game.config.width * percentaje, 50);
+        })
+    
+        this.load.on('complete', () => {
+            console.log("cargo!");
+            this.scene.start("level0");
+        })
     }
     
     create(){}
